48969484d341bd4bd3ea9aed682afda90616f489
Mohit Bhimrajka <mbhimrajka@supervity.ai>
2025-06-07 21:41:50 +0530

    first commit

--- agent.py
+++ agent.py
@@ -1,8 +1,7 @@
 from google.adk.agents import Agent
 
 root_agent = Agent(
-    model='<FILL_IN_MODEL>',
-    name='root_agent',
-    description='A helpful assistant for user questions.',
-    instruction='Answer user questions to the best of your knowledge',
+    name="agentops_echo",
+    model="gemini-2.0-flash-001",
+    instruction="You are an echo bot. Repeat exactly what the user says."
 )
